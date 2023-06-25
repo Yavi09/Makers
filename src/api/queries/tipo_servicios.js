1ea530663187282e193dc8e4b9ef37ea6b631fc2
Yavi <xavi.padilla1906@gmail.com>
2023-06-25 10:54:11 -0600

    query tipo servicio completado (dudo el nombre de la tabla si es el correcto)

---
+++ src/api/queries/tipo_servicios.js
@@ -0,0 +1,41 @@
+// requerir el modulo con los attrs de la conexión
+const POOL = require('../db');
+
+// metodo para obtener los tipos de servicios
+// req (obtiene parametros de consulta)
+// res (retorna valor segun resultado)
+const get = async (req, res) => {
+    try {
+        //realizar consulta
+     //   const tiposervicios = await POOL.query('SELECT * FROM cargos');
+        //verificar el estado
+        if (res.status(200)) res.json(tiposervicios.rows)     
+    } catch (e) {
+        console.error(e.message);
+    }
+}
+
+/** Método para guardar cargos de los empleados
+ * req, datos enviados del front
+ * res, respuesta del servidor
+ */
+const store = async (req, res) => {
+    try {
+        //asignar al arreglo los valores del req
+        const { cargo } = req.body;
+        // preparando query con los datos
+        POOL,query('INSERT INTO TipoServicios(tipo_servicio) VALUES ($1)', [tiposervicios],
+        //funcion
+        (err, result) => {
+            //verificar si hay error
+            if (err) {
+                res.json({error:err.message});
+            }
+            //si no existe error, enviado exitosamente
+            res.status(201).send('tipo servicio agregado' + 'INSERT INTO TipoServicio(tipo_servicio) VALUES ($1)'
+            + [tiposervicios]);
+        })
+    } catch (error) {
+        console.error(error);
+    }
+}
